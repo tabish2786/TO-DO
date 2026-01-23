49d223c4681488b34dfa3d5d2c5b5c02377163ef
Tabish Ansari <iitian.tabish@gmail.com>
2026-01-23 14:57:13 +0530

    Updated code

--- todo.py
+++ todo.py
@@ -10,9 +10,16 @@ while True:
     Z = input("CHOOSE ANY OF THE OPTION:")
 
     if Z == "1":
-        T = input("ENTER THE NEW TASK")
+        T = input("ENTER THE NEW TASK:")
         if T != "":
             todo.append(T)
             print("TASK ADDED")
         else:
             print("TASK CAN'T BE ADDED")
+    elif Z == "2":
+        if len(todo) == 0:
+            print("NO TASKS AVAILABLE")
+        else:
+            print("YOUR TASKS:")
+            for i in range(len(todo)):
+                print(i + 1, ".", todo[i])
