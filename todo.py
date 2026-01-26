c3ebbb3c5296be57d801d7124875e2773608846f
Tabish Ansari <iitian.tabish@gmail.com>
2026-01-26 23:20:13 +0530

    Final Code for TODO

--- todo.py
+++ todo.py
@@ -5,8 +5,7 @@ while True:
     print("1. ADD NEW TASKS")
     print("2. VIEW ALL TASKS")
     print("3. REMOVE SOME TASKS")
-    print("4. VIEW COMPLETED TASKS")
-    print("5. EXIT")
+    print("4. EXIT")
     Z = input("CHOOSE ANY OF THE OPTION:")
 
     if Z == "1":
@@ -16,6 +15,7 @@ while True:
             print("TASK ADDED")
         else:
             print("TASK CAN'T BE ADDED")
+
     elif Z == "2":
         if len(todo) == 0:
             print("NO TASKS AVAILABLE")
@@ -23,6 +23,7 @@ while True:
             print("YOUR TASKS:")
             for i in range(len(todo)):
                 print(i + 1, ".", todo[i])
+
     elif Z == "3":
         if len(todo) == 0:
             print("NO TASKS TO REMOVE")
@@ -38,12 +39,7 @@ while True:
             else:
                 print("INVALID TASK NUMBER")
 
-
-
-
-
-    
-    elif Z == "5":
+    elif Z == "4":
         print("EXITING TODO APP")
         break
 
